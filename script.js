7fabe00cdd448a207d1adac93cc4721b9563b5cf
lutchenca <lutchenca@hotmail.com>
2021-03-07 17:21:01 -0300

    joguinho concluído sem estilização

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let food = {
 }
 
 function criarCobrinha() {
-    for(i=0; i < snake.length; i++){
+    for(i=0; i < snake.length; i++){ // i = corpo da cobra, cabeça é posicao 0 e corpo é 1
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
@@ -44,6 +44,13 @@ function iniciarJogo () {
    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;   
    if(snake[0].y > 15 *box && direction == "down") snake[0].y = 0;   
    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;   
+
+   for(i = 1; i < snake.length; i++){
+       if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+           clearInterval(jogo);
+           alert('GAME OVER :(');
+       }
+   }
 
     criarBG();
     criarCobrinha();
